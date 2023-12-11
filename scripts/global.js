9db8c6b79b27b3678185a3e900a4533d18d7216d
3lanceTyler <153377435+3lanceTyler@users.noreply.github.com>
2023-12-11 16:42:37 +0000

    Update global.js

---
+++ scripts/global.js
@@ -0,0 +1,11 @@
+function keyInfo () {
+    let $siteTitle = "Synchrevent";
+    let $siteQuote = "Synchronising Your Events!";
+    let $copyrightY = "2023";
+    document.getElementById('siteTitle').textContent = $siteTitle;
+    document.getElementById('siteQuote').textContent = $siteQuote;
+    document.getElementById('CRsiteTitle').textContent = $siteTitle;
+    document.getElementById('CRYear').textContent = $copyrightY;
+}
+
+keyInfo();
